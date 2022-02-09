0ec71be7391b7a2e6527e7775a0e06949c4f0281
jacob knudsen <jhakeknudsen@protonmail.com>
2022-02-09 14:19:48 -0700

    final commit calc

--- calc.js
+++ calc.js
@@ -10,7 +10,7 @@ const reader = readline.createInterface({
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
-	mathSymbol = tokens[0];
+	mathSymbol = tokens[0];                                  
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
